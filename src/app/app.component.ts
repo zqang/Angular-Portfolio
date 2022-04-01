aa6a44697a57b7d0f708eef2cc989569d31c897e
Zqang <alvinang1406@gmail.com>
2022-04-01 19:54:15 +0800

    changing responsive layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-website';
+
+  // constructor(ngZone: NgZone){}
+
+  // eat(){
+  //   NgZone.isInAngularZone
+  // }
 }
