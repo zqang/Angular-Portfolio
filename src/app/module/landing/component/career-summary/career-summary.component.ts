39640ce81d88f3ee3e7c193a5a31d9db697489fe
zqang <alvinang1406@gmail.com>
2023-09-09 22:35:14 +0800

    add in contact form as button

--- src/app/module/landing/component/career-summary/career-summary.component.ts
+++ src/app/module/landing/component/career-summary/career-summary.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngxs/store';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-career-summary',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./career-summary.component.scss']
 })
 export class CareerSummaryComponent implements OnInit {
+  isContactModalVisible = false;
+  contactForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private modal: NzModalService,
+    private fb: FormBuilder,
+    private store: Store) { }
 
   ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      email: [null, [Validators.required]],
+      enquiry: [null, [Validators.required]],
+    });
+  }
+
+  openContactFormModal() : void {
+    this.isContactModalVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isContactModalVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isContactModalVisible = false;
+  }
+
+
+
+  submitForm(): void {
+    if (this.contactForm.valid) {
+      // this.store.dispatch();
+      console.log('send email function checked')
+    } else {
+      Object.values(this.contactForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
   }
 
 }
