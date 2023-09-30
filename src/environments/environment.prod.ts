60de75f04088cc5dcdeb4e1bae01b441539ed734
zqang <alvinang1406@gmail.com>
2023-09-30 10:27:28 +0800

    set production equal to true

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  BACKEND_URL: 'https://portfolioapi2023.azurewebsites.net/api',
+  JWT_ALLOWED_DOMAIN: 'portfolioapi2023.azurewebsites.net'
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@
 // };
 
 export const environment = {
-  production: false,
+  production: true,
   BACKEND_URL: 'https://portfolioapi2023.azurewebsites.net/api',
   JWT_ALLOWED_DOMAIN: 'portfolioapi2023.azurewebsites.net'
 }
